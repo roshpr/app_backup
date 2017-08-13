4d0746426396cf44f8e71ea38a1bc12fd598cbc4
Rosh PR <roshpr@juniper.net>
2017-08-13 11:43:57 +0530

    Changing file paths

--- sync_nodes.sh
+++ sync_nodes.sh
@@ -10,9 +10,9 @@ do
    rsync -P /etc/apt/sources.list root@$node_ip:/etc/apt/sources.list
    rsync -rLP  ./agent/* root@$node_ip:/usr/local/backup/agent/
    #ssh root@$node_ip 'pip install --find-links=https://pypi.python.org/simple/ -r /usr/local/backup/agent/requirements.txt'
-   ssh root@$node_ip 'unlink /etc/init/csobackuprestore.conf'
-   ssh root@$node_ip 'cp /usr/local/backup/agent/csobackuprestore.conf /etc/init/; mkdir -p /backup'
-   ssh root@$node_ip 'service csobackuprestore restart'
+   ssh root@$node_ip 'unlink /etc/init/backuprestore.conf'
+   ssh root@$node_ip 'cp /usr/local/backup/agent/backuprestore.conf /etc/init/; mkdir -p /backup'
+   ssh root@$node_ip 'service backuprestore restart'
    ssh root@$node_ip 'sed -i "/^path.repo.*$/d" /etc/elasticsearch/elasticsearch.yml'
    ssh root@$node_ip 'echo "path.repo: [\"/backup\"]" >> /etc/elasticsearch/elasticsearch.yml; service elasticsearch restart'
 done
