fb9b6c9d9b3e0de5271ed7d714111b8cb276a060
Rosh PR <roshpr@juniper.net>
2017-08-13 11:33:58 +0530

    constants

---
+++ agent/constants.py
@@ -0,0 +1,5 @@
+BACK_UP_DIR = '/backup'
+RESTORE_DIR = '/restore'
+DUMP_DATA_DIR = '/datadump'
+CASSANDRA_DATA_DIR = '/var/lib/cassandra'
+APPS = ('cassandra', 'redis', 'etcd', 'arangodb', 'elasticsearch', 'mariadb', 'zookeeper', 'swift')
